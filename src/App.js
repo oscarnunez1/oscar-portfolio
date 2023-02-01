043e4a284e28b96e6b09c43f2f64188df2ae5701
Oscar Nunez <requinto31@gmail.com>
2023-01-31 19:26:51 -0500

    remove Route in between BrowserROuter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -10,23 +9,16 @@ import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='resume' element={<Resume />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
