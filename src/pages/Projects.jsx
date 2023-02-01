66272e7f069fb679c98016dd9fbb14a54d1e7555
Oscar Nunez <requinto31@gmail.com>
2023-01-31 22:17:55 -0500

    create mapping function in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,18 @@
+import projectsData from "../data/projects"
+
 function Projects() {
+  console.log(projectsData)
   return (
+    <>
       <h1>Projects</h1>
+      <ul>
+        {projectsData.map(project =>
+          <li key={project.id}>
+            {project.title}
+          </li>
+        )}
+      </ul>
+    </>
   )
 }
 
