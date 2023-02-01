c11e66f25617a1732046bfcced79cc255feef12e
Oscar Nunez <requinto31@gmail.com>
2023-01-31 19:05:58 -0500

    import route and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
+
 
 function App() {
   return (
