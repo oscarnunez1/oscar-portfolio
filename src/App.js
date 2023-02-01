e74a2ac71f349b0b000aec5a0817c9316ce6d803
Oscar Nunez <requinto31@gmail.com>
2023-01-31 19:46:32 -0500

    create, import NavBar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
