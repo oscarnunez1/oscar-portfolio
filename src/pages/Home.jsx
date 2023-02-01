4f0815999a3075e15e10d296c712c65e1bb09305
Oscar Nunez <requinto31@gmail.com>
2023-01-31 20:09:24 -0500

    build home function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 function Home() {
   return (
+    <>
       <h1>Home</h1>
+      <main>
+        <h1>Oscar Nunez</h1>
+        <h2>Full Stack Software Developer</h2>
+        <img src="https://i.imgur.com/VegtFwc.png" alt="Oscar Nunez"/>
+      </main>
+    </>
   )
 }
 
