44585a7c0d9ced4d6b0157d8812e81dc2a5d570f
Oscar Nunez <requinto31@gmail.com>
2023-01-31 20:38:12 -0500

    add to contact function

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,9 @@
 function Contact() {
   return (
+    <>
       <h1>Contact</h1>
+      <p>You can reach me anytime via email or LinkedIn for further discussions or information, and heres a link to my GitHub. I look forward to connecting with you.</p>
+    </>
   )
 }
 
