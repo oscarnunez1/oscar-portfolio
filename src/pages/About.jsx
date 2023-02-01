158ad173069a648be2beb99bd54ecc85224b15be
Oscar Nunez <requinto31@gmail.com>
2023-01-31 20:20:45 -0500

    add to about function

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,11 @@
 function About() {
   return (
+    <>
       <h1>About</h1>
+      <img src="https://i.imgur.com/VegtFwc.png" alt="Oscar Nunez" width="400" height="400"/>
+      <p>I am a full stack developer with a strong foundation in JavaScript, HTML, CSS, Node.js, Express, and MongoDB, coming from a diverse background in architectural and construction photography, journalism, drone operations, and social media management. I am excited to bring my unique perspective and skills to web development and build innovative and user-friendly applications.</p>
+
+    </>
   )
 }
 
