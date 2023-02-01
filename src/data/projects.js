7e856b2e4287d10a0a86904b1d3aeadf6a486b02
Oscar Nunez <requinto31@gmail.com>
2023-01-31 21:51:24 -0500

    fix image links in projectsData

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ const projectsData = [
   {
     title: "Bitconnect 4",
     description: "Bitconnect 4 is a bitcoin-themed remake of the original Connect Four two-player game launched in 1974. The game involves two players placing yellow or red bitcoin tokens down 7 columns comprised of 6 slots each. With a board total of 42 slots, players need to play strategically to connect 4 tokens either vertically, horizontally, or diagonally. The first one to Connect 4 wins the game!",
-    image: "/images/bitconnect-4.png",
+    image: <img src="../images/bitconnect-4.png" alt="bitconnect-4" />,
     repositoryLink: "https://github.com/oscarnunez1/bitconnect-4.git",
     deploymentLink: "https://oscarnunez1-bitconnect-4.netlify.app/"
   },
   {
     title: "Cityscape Social",
     description: "Welcome to Cityscape Social, your source for the latest and greatest in architecture, construction, and real estate development news. This social blog is a platform where users can share stunning pictures, news, and updates from the world of architecture and construction.",
-    image: "/images/cityscape-social.png",
+    image: <img src="../images/cityscape-social.png" alt="cityscape-social" />,
     repositoryLink: "https://github.com/oscarnunez1/cityscape-social-app.git",
     deploymentLink: "https://cityscape-social.fly.dev/"
   },
   {
     title: "Tic-Tac-Toe",
     description: "Tic Tac Toe is a 2-player game played on a 3x3 grid. Players take turns marking a square with X or O, and the goal is to get 3 in a row horizontally, vertically, or diagonally. If all squares are filled and no one wins, it's a draw.",
-    image: "/images/tic-tac-toe.png",
+    image: <img src="../images/tic-tac-toe.png" alt="tic-tac-toe" />,
     repositoryLink: "https://github.com/oscarnunez1/ttt-weekend.git",
     deploymentLink: "https://oscarnunez1-tic-tac-toe.netlify.app/"
   }
