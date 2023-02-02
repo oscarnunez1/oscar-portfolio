bc95eab04550da679edbf3467627405a1e2629de
Oscar Nunez <requinto31@gmail.com>
2023-02-01 23:25:46 -0500

    add netlify.toml

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -4,13 +4,14 @@ import findProject from "../utilties/findProject.js"
 function ProjectDetails() {
   const { projectDetails } = useParams()
   const projectData = findProject(projectDetails)
+  console.log(projectData);
   return (
     <>
       <h1>{projectData.title}</h1>
-      <img src={projectData.image} alt={projectData.title}></img>
       <p>{projectData.description}</p>
-      <a href={projectData.repoLink}><button>View the code</button></a>
-      <a href={projectData.deploymentLink}><button>Explore App</button></a>
+      <img src={projectData.image} alt={projectData.title} />
+      <a href={projectData.repositoryLink}><button>GitHub Repo</button></a>
+      <a href={projectData.deploymentLink}><button>Test The App</button></a>
     </>
   )
 }
