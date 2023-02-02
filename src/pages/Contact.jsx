85573f15195bd37014360dc9cebeb9cd2052f05f
Oscar Nunez <requinto31@gmail.com>
2023-02-02 00:03:23 -0500

    update email, linkedin, github href links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <>
       <h1>Contact</h1>
-      <p>You can reach me anytime via <span></span>email or LinkedIn for further discussions or information, and heres a link to my GitHub. I look forward to connecting with you.</p>
+      <p>You can reach me anytime via <a href="mailto:oscar.nunezcaba@gmail.com">email</a> or <a href="https://www.linkedin.com/in/oscarnunez34/">LinkedIn</a> for further discussions or information, and heres a link to my <a href="https://github.com/oscarnunez1">GitHub</a>. I look forward to connecting with you.</p>
     </>
   )
 }
