02f595a19c2b4c5d775b0e74059f89e8a6b5987b
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-21 17:17:35 -0500

    refine projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import projectsData from '../data/projects';
 import ProjectsList from '../components/ProjectsList';
@@ -14,15 +14,6 @@ function Projects() {
   const projects = useMemo(() => projectsData, []);
   const projectsList = useMemo(() => <ProjectsList projects={projects} />, [projects]);
 
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    margin: 'auto',
-    maxWidth: 800,
-    padding: '32px 16px',
-  };
-
   const descriptionStyle = {
     marginTop: '32px',
     marginBottom: '16px',
@@ -30,14 +21,12 @@ function Projects() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{containerStyle, width: '90vw'}}>
-        <Box sx={descriptionStyle}>
-          <Typography variant="body1" component="p" align="center">
-            Check out some of the projects I've worked on! Click "Learn More" to see more details about each project.
-          </Typography>
-        </Box>
-        <div>{projectsList}</div>
-      </Box>
+      <Container maxWidth="md">
+        <Typography variant="body1" component="p" align="center" sx={descriptionStyle}>
+          Check out some of the projects I've worked on! Click "Learn More" to see more details about each project.
+        </Typography>
+        {projectsList}
+      </Container>
     </ThemeProvider>
   );
 }
